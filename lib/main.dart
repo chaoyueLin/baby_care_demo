5d73a3dd9f1bf0812a25df66fd938c7853a147c8
chaoyueLin <chaoyue.lin@qq.com>
2024-09-04 22:08:52 +0800

    增加侧边栏的使用

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,20 @@ import 'pages/home_page.dart';
 import 'pages/search_page.dart';
 import 'pages/notifications_page.dart';
 import 'pages/profile_page.dart';
+import 'package:flutter_side_menu/flutter_side_menu.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
@@ -34,38 +28,78 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tab Example'),
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.blue,
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/home.png', width: 24, height: 24),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/search.png', width: 24, height: 24),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/notifications.png', width: 24, height: 24),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/profile.png', width: 24, height: 24),
-            label: 'Profile',
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Side Menu Example'),
+        ),
+        body: Row(
+          children: [
+            SideMenu(
+              builder: (data) {
+                return SideMenuData(
+                  items: [
+                    SideMenuItemDataTile(
+                      isSelected: _currentIndex == 0,
+                      title: 'Home',
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 0;
+                        });
+                      },
+                      icon: const Icon(Icons.home),
+                      selectedIcon: const Icon(Icons.home, color: Colors.black),
+                      titleStyle: TextStyle(color: Colors.blue),
+                      selectedTitleStyle: TextStyle(color: Colors.black),
+                    ),
+                    SideMenuItemDataTile(
+                      isSelected: _currentIndex == 1,
+                      title: 'Search',
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 1;
+                        });
+                      },
+                      icon: const Icon(Icons.search),
+                      selectedIcon: const Icon(Icons.search, color: Colors.black),
+                      titleStyle: TextStyle(color: Colors.blue),
+                      selectedTitleStyle: TextStyle(color: Colors.black),
+                    ),
+                    SideMenuItemDataTile(
+                      isSelected: _currentIndex == 2,
+                      title: 'Notifications',
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 2;
+                        });
+                      },
+                      icon: const Icon(Icons.notifications),
+                      selectedIcon: const Icon(Icons.notifications, color: Colors.black),
+                      titleStyle: TextStyle(color: Colors.blue),
+                      selectedTitleStyle: TextStyle(color: Colors.black),
+                    ),
+                    SideMenuItemDataTile(
+                      isSelected: _currentIndex == 3,
+                      title: 'Profile',
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 3;
+                        });
+                      },
+                      icon: const Icon(Icons.person),
+                      selectedIcon: const Icon(Icons.person, color: Colors.black),
+                      titleStyle: TextStyle(color: Colors.blue),
+                      selectedTitleStyle: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                );
+              },
+            ),
+            Expanded(
+              child: _pages[_currentIndex], // Displays the current selected page
+            ),
+          ],
+        ),
       ),
     );
   }
