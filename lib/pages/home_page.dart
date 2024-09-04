665f7c8569c5f451da027b704b4ebf9bc78ac3c9
chaoyueLin <chaoyue.lin@qq.com>
2024-09-04 21:30:40 +0800

    增加本地图片选择

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page', style: TextStyle(fontSize: 24)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _image == null
+            ? Text('No image selected.', style: TextStyle(fontSize: 18))
+            : Image.file(_image!, height: 200, width: 200, fit: BoxFit.cover),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: _pickImage,
+          child: Text('Pick Image from Gallery'),
+        ),
+      ],
     );
   }
 }
