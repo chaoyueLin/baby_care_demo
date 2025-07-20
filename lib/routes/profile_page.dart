433dce7a7bca9fcee51dae08d835e09576ad0ccf
chaoyue.lin <chaoyue.lin@qq.com>
2025-07-20 08:02:43 +0800

    去掉枚举

--- lib/routes/profile_page.dart
+++ lib/routes/profile_page.dart
@@ -5,11 +5,58 @@ import 'package:flutter_gen/gen_l10n/S.dart';
 
 import '../widget/custom_tab_button.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   final List<Color> lineColors = [Colors.blue, Colors.red, Colors.green];
+
+  List<List<FlSpot>> selectedData = GrowStandard.girlWeight0to13WeekData;
 
   @override
   Widget build(BuildContext context) {
+    double maxX=0.0, minX = 0.0, minY=0.0, maxY=0.0, intervalX=0.0, intervalY=0.0;
+
+    if (selectedData == GrowStandard.girlWeight0to13WeekData||selectedData==GrowStandard.boyWeight0to13WeekData) {
+      maxX = 13.0;
+      minY = 2;
+      maxY = 8.5;
+      intervalX = 1;
+      intervalY = 0.5;
+    } else if (selectedData == GrowStandard.girlWeight0to6MonthData|| selectedData==GrowStandard.boyWeight0to6MonthData) {
+      maxX = 60.0;
+      minY = 2.0;
+      maxY = 25.0;
+      intervalX = 5;
+      intervalY = 1.0;
+    } else if (selectedData == GrowStandard.girlHeight0to13WeekData|| selectedData==GrowStandard.boyHeight0to13WeekData) {
+      maxX = 13.0;
+      minY = 45.0;
+      maxY = 66.0;
+      intervalX = 1;
+      intervalY = 1.0;
+    } else if(selectedData == GrowStandard.girlHeight0to24MonthData|| selectedData==GrowStandard.boyHeight0to24MonthData){
+      maxX = 24.0;
+      minY = 45.0;
+      maxY = 94.0;
+      intervalX = 1;
+      intervalY = 1.0;
+    }else if (selectedData == GrowStandard.girlBMI0to13WeekData|| selectedData==GrowStandard.boyBMI0to13WeekData) {
+      maxX = 13.0;
+      minY = 11.0;
+      maxY = 20.0;
+      intervalX = 1;
+      intervalY = 1.0;
+    } else if(selectedData == GrowStandard.girlBMI0to24MonthData|| selectedData==GrowStandard.boyBMI0to24MonthData){
+      maxX = 24.0;
+      minY = 11.0;
+      maxY = 20.0;
+      intervalX = 1;
+      intervalY = 1.0;
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -20,24 +67,31 @@ class ProfilePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 CustomTabButton(
-                  label: S.of(context)?.breastMilk ?? "breastMilk",
-                  iconPath: 'assets/icons/mother_milk.png',
-                  onTap: () {},
-                ),
-                CustomTabButton(
                   label: S.of(context)?.formula ?? "formula",
                   iconPath: 'assets/icons/formula_milk.png',
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                      selectedData = GrowStandard.girlWeight0to13WeekData;
+                    });
+                  },
                 ),
                 CustomTabButton(
                   label: S.of(context)?.water ?? "water",
                   iconPath: 'assets/icons/water.png',
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                      selectedData = GrowStandard.girlHeight0to13WeekData;
+                    });
+                  },
                 ),
                 CustomTabButton(
                   label: S.of(context)?.poop ?? "poop",
                   iconPath: 'assets/icons/poop.png',
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                      selectedData = GrowStandard.girlBMI0to13WeekData;
+                    });
+                  },
                 ),
               ],
             ),
@@ -45,16 +99,13 @@ class ProfilePage extends StatelessWidget {
           Expanded(
             flex: 6,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
               child: LineChart(
                 LineChartData(
                   lineTouchData: LineTouchData(enabled: false),
                   titlesData: FlTitlesData(
                     leftTitles: AxisTitles(
-                      axisNameWidget: Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: Text("体重 (kg)"),
-                      ),
                       sideTitles: SideTitles(
                         showTitles: true,
                         reservedSize: 40,
@@ -66,17 +117,15 @@ class ProfilePage extends StatelessWidget {
                         },
                       ),
                     ),
-                    rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                    topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    rightTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    topTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
                     bottomTitles: AxisTitles(
-                      axisNameWidget: Padding(
-                        padding: const EdgeInsets.only(top: 2.0),
-                        child: Text("年龄 (周)"),
-                      ),
                       sideTitles: SideTitles(
                         showTitles: true,
                         reservedSize: 32,
-                        interval: 1,
+                        interval: intervalX,
                         getTitlesWidget: (value, meta) {
                           return Padding(
                             padding: const EdgeInsets.only(top: 0.0),
@@ -90,18 +139,18 @@ class ProfilePage extends StatelessWidget {
                   gridData: FlGridData(
                     show: true,
                     drawVerticalLine: true,
-                    horizontalInterval: 0.5,
-                    verticalInterval: 1,
+                    horizontalInterval: intervalY,
+                    verticalInterval: intervalX,
                   ),
-                  minX: 0,
-                  maxX: 13,
-                  minY: 2.5,
-                  maxY: 8.5,
+                  minX: minX,
+                  maxX: maxX,
+                  minY: minY,
+                  maxY: maxY,
                   lineBarsData: List.generate(
-                    GrowStandard.girlWeight0to13WeekData.length,
-                        (index) => _lineChartBarData(
-                      GrowStandard.girlWeight0to13WeekData[index],
-                      lineColors[index],
+                    selectedData.length,
+                    (index) => _lineChartBarData(
+                      selectedData[index],
+                      lineColors[index % lineColors.length],
                     ),
                   ),
                 ),
